72f62362b763673ef05eae2e8c70a0970d07ce2d
Dhaval Gupta <guptadhaval18@gmail.com>
2021-03-06 04:59:57 +0530

    added custom/broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+let socket = io();
+
+socket.on('connect', function(){
+    console.log("Connected to server");
+});
+
+socket.on('newMessage', function(message){
+    console.log("newMessage", message);
+});
+
+socket.on('disconnect', function(){
+    console.log("Disconnected from server");
+});
--- server/server.js
+++ server/server.js
@@ -12,14 +12,35 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
-    console.log("A new user just connected")
+    console.log("A new user just connected");
+
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app!",
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user Joined!",
+        createdAt: new Date().getTime()
+    })
+    
+    socket.on('createMessage', (message)=>{
+        console.log("createMessage", message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    });
 
     socket.on('disconnect', ()=>{
-        console.log("User got disconnected")
+        console.log("User got disconnected");
     });
 });
 
 
 server.listen(port, ()=>{
-    console.log(`Server is up on port ${port}`)
+    console.log(`Server is up on port ${port}`);
 });
